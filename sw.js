7d7e329983ebc781f5941971957c53ebdc23ec87
nkarthik91 <karthikwelcomesall@gmail.com>
2026-09-04 19:32:29 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,62 +1,11 @@
-const CACHE_NAME = "kg-farms-v1";
-
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./manifest.json"
-];
-
-
-self.addEventListener("install", event => {
-
-  event.waitUntil(
-
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(FILES_TO_CACHE);
-      })
-
-  );
-
-  self.skipWaiting();
-
-});
-
-
-self.addEventListener("activate", event => {
-
-  event.waitUntil(
-
-    caches.keys().then(cacheNames => {
-
-      return Promise.all(
-
-        cacheNames
-          .filter(name => name !== CACHE_NAME)
-          .map(name => caches.delete(name))
-
-      );
-
-    })
-
-  );
-
-  self.clients.claim();
-
-});
-
-
-self.addEventListener("fetch", event => {
-
-  event.respondWith(
-
-    fetch(event.request)
-      .catch(() => {
-
-        return caches.match(event.request);
-
-      })
-
-  );
-
+const CACHE='kg-farms-shell-v1';
+const SHELL=['./','./index.html'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{
+  const r=e.request;
+  if(r.method!=='GET') return;
+  const u=new URL(r.url);
+  if(u.origin!==self.location.origin) return;
+  e.respondWith(caches.match(r).then(cached=>cached||fetch(r).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(r,copy));return res}).catch(()=>cached)));
 });
